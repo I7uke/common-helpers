e2ab39d363fb1ed63d937fbbe23a364bf799abc0
Ivan Malahov <malakhov.i.v.a.n@mail.ru>
2025-03-23 23:09:06 +0300

    Обновлен getLastIdFromUrl

--- __tests__/getLastIdFromUrl.test.ts
+++ __tests__/getLastIdFromUrl.test.ts
@@ -1,318 +1,87 @@
 import getLastIdFromUrl from "../src/getLastIdFromUrl";
 
-interface TestUrl {
-    readonly url: string;
-    readonly idString?: string | undefined | null;
-    readonly idNumber?: number | undefined | null;
-}
-
-function getTestUrl1(): TestUrl {
-    return {
-        idNumber: 5,
-        idString: '5',
-        url: 'http://path1/path2/5'
+function testValidValue(url: string, resultString: string, resultNumber?: number) {
+    // toString
+    expect(getLastIdFromUrl.toString(url)).toStrictEqual(resultString);
+    expect(getLastIdFromUrl.toString(url, undefined)).toStrictEqual(resultString);
+    expect(getLastIdFromUrl.toString(url, null)).toStrictEqual(resultString);
+    expect(getLastIdFromUrl.toString(url, 'defaultValue')).toStrictEqual(resultString);
+    // toNumber
+    const isCanBeNumber: boolean = typeof resultNumber === 'number';
+    if(isCanBeNumber) {
+        expect(getLastIdFromUrl.toNumber(url)).toStrictEqual(resultNumber);
     }
+    expect(getLastIdFromUrl.toNumber(url, undefined)).toStrictEqual(isCanBeNumber ? resultNumber: undefined);
+    expect(getLastIdFromUrl.toNumber(url, null)).toStrictEqual(isCanBeNumber ? resultNumber: null);
+    expect(getLastIdFromUrl.toNumber(url, 7)).toStrictEqual(isCanBeNumber ? resultNumber: 7);
 }
 
-function getTestUrl2(): TestUrl {
-    return {
-        idString: 'string',
-        url: 'http://path1/path2/string'
+test('Некорректные значения', () => {
+    const incorrectValues: any[] = [
+        true,
+        () => { },
+        { a: 1, b: 2 },
+        [1, 2, 3],
+        0,
+        10,
+        NaN
+    ];
+
+    for(const value of incorrectValues) {
+        // toString
+        expect(getLastIdFromUrl.toString(value)).toStrictEqual('');
+        expect(getLastIdFromUrl.toString(value, undefined)).toStrictEqual(undefined);
+        expect(getLastIdFromUrl.toString(value, null)).toStrictEqual(null);
+        expect(getLastIdFromUrl.toString(value, 'defaultValue')).toStrictEqual('defaultValue');
+        // toNumber
+        expect(getLastIdFromUrl.toNumber(value)).toStrictEqual(0);
+        expect(getLastIdFromUrl.toNumber(value, undefined)).toStrictEqual(undefined);
+        expect(getLastIdFromUrl.toNumber(value, null)).toStrictEqual(null);
+        expect(getLastIdFromUrl.toNumber(value, 5)).toStrictEqual(5);
     }
-}
+});
 
-function getTestUrl3(): TestUrl {
-    return {
-        idString: '5',
-        idNumber: 5,
-        url: 'http://path1/path2/ 5 '
+test('Пустое значение', () => {
+    const incorrectValues: any[] = [
+        '',
+        ' ',
+        'url',
+        'http://path1/path2/',
+        'http://path1/path2/ ',
+        undefined,
+        null
+    ];
+
+    for(const value of incorrectValues) {
+        // toString
+        expect(getLastIdFromUrl.toString(value)).toStrictEqual('');
+        expect(getLastIdFromUrl.toString(value, undefined)).toStrictEqual(undefined);
+        expect(getLastIdFromUrl.toString(value, null)).toStrictEqual(null);
+        expect(getLastIdFromUrl.toString(value, 'defaultValue')).toStrictEqual('defaultValue');
+        // toNumber
+        expect(getLastIdFromUrl.toNumber(value)).toStrictEqual(0);
+        expect(getLastIdFromUrl.toNumber(value, undefined)).toStrictEqual(undefined);
+        expect(getLastIdFromUrl.toNumber(value, null)).toStrictEqual(null);
+        expect(getLastIdFromUrl.toNumber(value, 5)).toStrictEqual(5);
     }
-}
-
-function getTestUrl4(): TestUrl {
-    return {
-        idString: 'string',
-        url: 'http://path1/path2/ string '
-    }
-}
-
-function getTestUrl5(): TestUrl {
-    return {
-        idString: '5Id',
-        url: 'http://path1/path2/5Id'
-    }
-}
-
-function getTestUrl6(): TestUrl {
-    return {
-        url: 'http://path1/path2/'
-    }
-}
-
-function getTestUrl7(): TestUrl {
-    return {
-        url: 'http://path1/path2/ '
-    }
-}
-
-function getTestUrl8(): TestUrl {
-    return {
-        url: 'path'
-    }
-}
-
-test('Некорректное значение - boolean', () => {
-    expect(getLastIdFromUrl({
-        url: true as any
-    })).toStrictEqual(undefined);
 });
 
-test('Некорректное значение - function', () => {
-
-    expect(getLastIdFromUrl({
-        url: (() => { }) as any
-    })).toStrictEqual(undefined);
+test('id число', () => {
+    testValidValue('http://path1/path2/5', '5', 5);
+    testValidValue('http://path1/path2/10', '10', 10);
+    testValidValue('http://path1/path2/125', '125', 125);
+    testValidValue('http://path1/path2/100500', '100500', 100500);
+    testValidValue('http://path1/path2/ 5', '5', 5);
+    testValidValue('http://path1/path2/ 5 ', '5', 5);
+    testValidValue('http://path1/path2/ 15 ', '15', 15);
 });
 
-test('Некорректное значение - object', () => {
-    expect(getLastIdFromUrl({
-        url: { a: 1, b: 2 } as any
-    })).toStrictEqual(undefined);
-});
-
-test('Некорректное значение - array', () => {
-    expect(getLastIdFromUrl({
-        url: [1, 2, 3] as any
-    })).toStrictEqual(undefined);
-});
-
-test('1. url: undefined', () => {
-    expect(getLastIdFromUrl({
-        url: undefined
-    })).toStrictEqual(undefined);
-});
-
-test('2. url: undefined', () => {
-    expect(getLastIdFromUrl({
-        url: undefined,
-        defaultValue: undefined
-    })).toStrictEqual(undefined);
-});
-
-test('3. url: undefined', () => {
-    expect(getLastIdFromUrl({
-        url: undefined,
-        defaultValue: null
-    })).toStrictEqual(null);
-});
-
-test('4. url: undefined', () => {
-    expect(getLastIdFromUrl({
-        url: undefined,
-        defaultValue: 'defaultValue',
-        convertTo: 'string'
-    })).toStrictEqual('defaultValue');
-});
-
-test('5. url: undefined', () => {
-    expect(getLastIdFromUrl({
-        url: undefined,
-        convertTo: 'number',
-        defaultValue: 10
-    })).toStrictEqual(10);
-});
-
-test('1. url: null', () => {
-    expect(getLastIdFromUrl({
-        url: null
-    })).toStrictEqual(undefined);
-});
-
-test('2. url: null', () => {
-    expect(getLastIdFromUrl({
-        url: null,
-        defaultValue: null
-    })).toStrictEqual(null);
-});
-
-test('3. url: null', () => {
-    expect(getLastIdFromUrl({
-        url: null,
-        defaultValue: null
-    })).toStrictEqual(null);
-});
-
-test('4. url: null', () => {
-    expect(getLastIdFromUrl({
-        url: null,
-        defaultValue: 'defaultValue',
-        convertTo: 'string'
-    })).toStrictEqual('defaultValue');
-});
-
-test('5. url: null', () => {
-    expect(getLastIdFromUrl({
-        url: null,
-        convertTo: 'number',
-        defaultValue: 10
-    })).toStrictEqual(10);
-});
-
-test('1. url: Пустая строка', () => {
-    expect(getLastIdFromUrl({
-        url: ''
-    })).toStrictEqual(undefined);
-});
-
-test('2. url: Пустая строка', () => {
-    expect(getLastIdFromUrl({
-        url: '',
-        defaultValue: null
-    })).toStrictEqual(null);
-});
-
-test('3. url: Пустая строка', () => {
-    expect(getLastIdFromUrl({
-        url: '',
-        defaultValue: null
-    })).toStrictEqual(null);
-});
-
-test('4. url: Пустая строка', () => {
-    expect(getLastIdFromUrl({
-        url: '',
-        defaultValue: 'defaultValue',
-        convertTo: 'string'
-    })).toStrictEqual('defaultValue');
-});
-
-test('5. url: Пустая строка', () => {
-    expect(getLastIdFromUrl({
-        url: '',
-        convertTo: 'number',
-        defaultValue: 10
-    })).toStrictEqual(10);
-});
-
-test('1. id число', () => {
-    const testValue = getTestUrl1();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'number'
-    })).toStrictEqual(testValue.idNumber);
-});
-
-test('2. id число', () => {
-    const testValue = getTestUrl1();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'string'
-    })).toStrictEqual(testValue.idString);
-});
-
-test('3. id число', () => {
-    const testValue = getTestUrl3();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'number'
-    })).toStrictEqual(testValue.idNumber);
-});
-
-test('4. id число', () => {
-    const testValue = getTestUrl3();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'string'
-    })).toStrictEqual(testValue.idString);
-});
-
-test('1. id строка', () => {
-    const testValue = getTestUrl2();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-    })).toStrictEqual(testValue.idString);
-});
-
-test('2. id строка', () => {
-    const testValue = getTestUrl2();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'string'
-    })).toStrictEqual(testValue.idString);
-});
-
-test('3. id строка', () => {
-    const testValue = getTestUrl2();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'number'
-    })).toStrictEqual(undefined);
-});
-
-test('4. id строка', () => {
-    const testValue = getTestUrl4();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-    })).toStrictEqual(testValue.idString);
-});
-
-test('5. id строка', () => {
-    const testValue = getTestUrl5();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-    })).toStrictEqual(testValue.idString);
-});
-
-test('6. id строка', () => {
-    const testValue = getTestUrl5();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'number'
-    })).toStrictEqual(undefined);
-});
-
-test('7. id строка', () => {
-    const testValue = getTestUrl5();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'number',
-        defaultValue: 100
-    })).toStrictEqual(100);
-});
-
-test('8. id строка', () => {
-    const testValue = getTestUrl6();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-    })).toStrictEqual(undefined);
-});
-
-test('9. id строка', () => {
-    const testValue = getTestUrl7();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-    })).toStrictEqual(undefined);
-});
-
-test('10. id строка', () => {
-    const testValue = getTestUrl8();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-    })).toStrictEqual(undefined);
-});
-
-test('11. id строка', () => {
-    const testValue = getTestUrl8();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        convertTo: 'number'
-    })).toStrictEqual(undefined);
-});
-
-test('12. id строка', () => {
-    const testValue = getTestUrl8();
-    expect(getLastIdFromUrl({
-        url: testValue.url,
-        defaultValue: 'defaultValue',
-    })).toStrictEqual('defaultValue');
+test('id строка', () => {
+    testValidValue('http://path1/path2/string', 'string');
+    testValidValue('http://path1/path2/t', 't');
+    testValidValue('http://path1/path2/ t ', 't');
+    testValidValue('http://path1/path2/ string', 'string');
+    testValidValue('http://path1/path2/ string ', 'string');
+    testValidValue('http://path1/path2/5id ', '5id');
+    testValidValue('http://path1/path2/id25 ', 'id25');
 });
--- src/getLastIdFromUrl.ts
+++ src/getLastIdFromUrl.ts
@@ -1,61 +1,47 @@
-type ConvertToString = 'string';
-type ConvertToNumber = 'number';
-type ConvertTo = ConvertToString | ConvertToNumber | undefined;
-type ConvertResult<T extends ConvertTo> = T extends ConvertToNumber ? number : string;
-type DefaultValueAvailableType = string | number | undefined | null;
-type DefaultValue<TConvertTo extends ConvertTo, TValue extends DefaultValueAvailableType> = TConvertTo extends ConvertToNumber ? Exclude<TValue, string> : Exclude<TValue, number>;
+function toString<T extends string | undefined | null = string>(url: string | undefined | null, defaultValue?: T) {
+    const defaultValueString: T | string = arguments.length <= 1 ? '' : defaultValue as T;
 
-interface Options<TConvertTo extends ConvertTo, TDefaultValue extends DefaultValueAvailableType> {
-    readonly url: string | undefined | null;
-    readonly defaultValue?: DefaultValue<TConvertTo, TDefaultValue>;
-    readonly convertTo?: TConvertTo;
-}
-
-export default function getLastIdFromUrl<TConvertTo extends ConvertTo = undefined, TDefaultValue extends DefaultValueAvailableType = undefined>(options: Options<TConvertTo, TDefaultValue>): ConvertResult<TConvertTo> | DefaultValue<TConvertTo, TDefaultValue> {
-    const url = options.url;
-    let convertTo: ConvertTo = 'string';
-    let defaultValue: DefaultValue<TConvertTo, TDefaultValue> | undefined = undefined;
-
-    if (options.convertTo === 'number') {
-        convertTo = 'number';
-    }
-
-    if (typeof options.defaultValue === 'string'
-        || typeof options.defaultValue === 'number'
-        || options.defaultValue === null
-        || options.defaultValue === undefined) {
-        defaultValue = options.defaultValue;
-    }
-
-    if (typeof url !== 'string') {
-        return defaultValue as DefaultValue<TConvertTo, TDefaultValue>;
-    }
-
-    if (!url) {
-        return defaultValue as DefaultValue<TConvertTo, TDefaultValue>;
+    if (typeof url !== 'string' || !url) {
+        return defaultValueString;
     }
 
     const index = url.lastIndexOf('/');
 
     if (index < 0) {
-        return defaultValue as DefaultValue<TConvertTo, TDefaultValue>;
+        return defaultValueString;
     }
 
     // Получаем id, +1, чтобы получить id без разделителя
     const idString = url.substring(index + 1).trim();
 
     if (!idString) {
-        return defaultValue as DefaultValue<TConvertTo, TDefaultValue>;
+        return defaultValueString;
     }
 
-    if (convertTo === 'number') {
-        const idNumber: number = Number(idString);
-        if (isNaN(idNumber)) {
-            return defaultValue as DefaultValue<TConvertTo, TDefaultValue>;
-        }
-
-        return idNumber as ConvertResult<TConvertTo>;
-    }
-
-    return idString as ConvertResult<TConvertTo>;
+    return idString;
 }
+
+function toNumber<T extends number | undefined | null = number>(url: string | undefined | null, defaultValue?: T): number | T {
+    const defaultValueNumber: T | number = arguments.length <= 1 ? 0 : defaultValue as T;
+    const idString = toString(url);
+
+    if(!idString) {
+        return defaultValueNumber;
+    }
+
+    const idNumber = Number(idString);
+    
+
+    if (isNaN(idNumber)) {
+        return defaultValueNumber;
+    }
+
+    return idNumber;
+}
+
+const getLastIdFromUrl = Object.freeze({
+    toNumber,
+    toString
+});
+
+export default getLastIdFromUrl
